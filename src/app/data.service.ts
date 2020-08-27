7f1766965ec600cfa473121325e5485eceb333c9
CraigA1987 <48647012+CraigA1987@users.noreply.github.com>
2020-08-27 13:35:31 +0100

    first Subject!

---
+++ src/app/data.service.ts
@@ -0,0 +1,86 @@
+import { Injectable } from "@angular/core";
+import { Trip } from "../app/models/trip.model";
+import { Receipt } from "../app/models/recipt.model";
+import { UserService } from "./user.service";
+import { BehaviorSubject } from "rxjs";
+import { map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root",
+})
+export class DataService {
+  constructor(private userService: UserService) {}
+  // userService injected to get user user email - used in the API calls this.userservice.user.email = users logged in email addess
+
+  // will hold an array of all fetched trips for a user from the db
+  private _trips = new BehaviorSubject<Trip[]>([
+    new Trip(
+      "craig_adam2k@hotmail.com",
+      "lax20200312",
+      "los Angeles",
+      "best RAF course ever!!!!",
+      new Date("2015-03-20"),
+      new Date(),
+      100
+    ),
+    new Trip(
+      "craigadam1987@gmail.com",
+      "jpn20200312",
+      "Japan",
+      "International Relations Exercise",
+      new Date("2015-03-25"),
+      new Date(),
+      400
+    ),
+    new Trip(
+      "craig_adam2k@hotmail.com",
+      "rky20200312",
+      "Iceland",
+      "Satcomms course",
+      new Date("2015-03-26"),
+      new Date(),
+      200
+    ),
+  ]);
+
+  get trips() {
+    return this._trips.asObservable(); // can now be subscribed to from other componenets
+  }
+
+  // will hold an array of all the fetches recipts for a user from db
+  private _recipts: Receipt[];
+
+  getTrips() {
+    // would fetch trips from the server... based on user email address
+    return this._trips;
+  }
+
+  getRecipts() {
+    return this._recipts;
+  }
+
+  updateTrip(
+    user: string,
+    tripId: string,
+    location: string,
+    description: string,
+    dateFrom: Date,
+    dateTo: Date,
+    price: number
+  ) {
+    // Create a new Trip object
+    let newTrip = new Trip(
+      user,
+      tripId,
+      location,
+      description,
+      dateFrom,
+      dateTo,
+      price
+    );
+    // emit a value on the this._trips event emitter
+    // get the current value of this._trips and add the newTrip onto it, creating a new value to emit
+    this._trips.next(this._trips.getValue().concat(newTrip));
+    // this._trips Trip array is now emitted out to all subscribers (the all-trips.page) to update the page automatically
+  }
+}
--- src/app/trips/all-trips/all-trips.page.ts
+++ src/app/trips/all-trips/all-trips.page.ts
@@ -1,21 +1,93 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input, OnDestroy } from "@angular/core";
 import { SegmentChangeEventDetail } from "@ionic/core";
+import { DataService } from "../../data.service";
+import { Trip } from "../../models/trip.model";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-all-trips",
   templateUrl: "./all-trips.page.html",
   styleUrls: ["./all-trips.page.scss"],
 })
-export class AllTripsPage implements OnInit {
-  constructor() {}
+export class AllTripsPage implements OnInit, OnDestroy {
+  constructor(private dataService: DataService) {}
+  retrievedTrips: Trip[];
+  orderedTrips: Trip[];
+  private tabsValue = "cost";
 
-  ngOnInit() {}
+  private tripsSubscription: Subscription;
+
+  ngOnInit() {
+    // start with relevant trips being ordered by Date
+    this.tripsSubscription = this.dataService.trips.subscribe((tripsArray) => {
+      this.retrievedTrips = tripsArray;
+      // orderedTrips array needs to be a seperate array in memory
+      this.orderedTrips = this.retrievedTrips.slice(0);
+      // each time we get an update to the array we need to re-order - firstly get the tab value, then call the onFilterLoad method to filter the tripsArray
+      this.tabsValue = document.querySelector("ion-segment").value;
+      this.onFilterLoad(this.tabsValue);
+    });
+  }
+
+  ionViewWillEnter() {
+    console.log("entering view");
+    // this.placesService.fetchPlaces().subscribe(() => { // will subscribe to changes
+    //   this.isLoading = false;
+    // });
+  }
 
   onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
+    console.log("filtering");
     if (event.detail.value === "recent") {
+      this.orderedTrips.sort((a, b) => {
+        if (a.dateFrom < b.dateFrom) return 1;
+        if (a.dateFrom > b.dateFrom) return -1;
+        return 0;
+      });
+    } else {
+      this.orderedTrips.sort((a, b) => {
+        if (a.price < b.price) return 1;
+        if (a.price > b.price) return -1;
+        return 0;
+      });
+    }
+  }
+
+  onFilterLoad(value: string) {
+    if (value === "recent") {
       console.log("most recent");
+      this.orderedTrips.sort((a, b) => {
+        if (a.dateFrom < b.dateFrom) return 1;
+        if (a.dateFrom > b.dateFrom) return -1;
+        return 0;
+      });
     } else {
       console.log("cost");
+      this.orderedTrips.sort((a, b) => {
+        if (a.price < b.price) return 1;
+        if (a.price > b.price) return -1;
+        return 0;
+      });
+    }
+  }
+
+  updateTest() {
+    console.log("upadte test pressed");
+    this.dataService.updateTrip(
+      "craig_adam2k@hotmail.com",
+      "tor20200312",
+      "Toronto",
+      "Messing up the Raptors!",
+      new Date("2010-03-30"),
+      new Date("2018-03-30"),
+      4000
+    );
+  }
+
+  ngOnDestroy() {
+    // If we have an active subscription, it should be removed to prevent any memory leak
+    if (this.tripsSubscription) {
+      this.tripsSubscription.unsubscribe();
     }
   }
 }
